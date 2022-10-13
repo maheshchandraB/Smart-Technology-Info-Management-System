30ec9803b155168b5ea589ac824d35a89d42904a
maheshchandraB <74002433+maheshchandraB@users.noreply.github.com>
2022-10-13 11:11:57 +0530

    Add files via upload

---
+++ build/precache-manifest.7e297b51f09ea0934cc888fb6df32cc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab136ba5d85351928369",
+    "url": "/static/css/main.20a0009c.chunk.css"
+  },
+  {
+    "revision": "ab136ba5d85351928369",
+    "url": "/static/js/main.ab136ba5.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "4eeefe41ecb62f6a6a34",
+    "url": "/static/js/2.4eeefe41.chunk.js"
+  },
+  {
+    "revision": "4431f52a260a7d5c73e4",
+    "url": "/static/js/runtime~main.4431f52a.js"
+  },
+  {
+    "revision": "6cc2cbdde5855bf0f41e3771b196ffd1",
+    "url": "/index.html"
+  }
+];
